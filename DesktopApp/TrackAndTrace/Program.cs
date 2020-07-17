0f20b1e7b4b2b835c005e05dff0cd15d9aa2279a
Bradley Davis <bradders.davis@gmail.com>
2020-07-17 17:55:03 +0100

    Change to C# project for better interaction with Windows

---
+++ DesktopApp/TrackAndTrace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrackAndTrace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
