75800a784fea256148afd98ac214ebf8507db650
Bradley Davis <bradders.davis@gmail.com>
2020-07-18 18:50:25 +0100

    Create TextMessage.cs to store each text message as it is read from file.

---
+++ DesktopApp/TrackAndTrace/TextMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TrackAndTrace
+{
+    public class TextMessage
+    {
+        public string Id { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+        public DateTime Time { get; set; }
+
+        public TextMessage(string id, string body, string from, DateTime time)
+        {
+            Id = id;
+            Body = body;
+            From = from;
+            Time = time;
+        }
+    }
+}
