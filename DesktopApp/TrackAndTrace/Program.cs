6ef5888f62cd0d84e87dcb6270a5168772c8efaa
Bradley Davis <bradders.davis@gmail.com>
2020-07-18 23:47:12 +0100

    Finish program.cs to run the tasks repeatedly with a 4 - 6 minute wait between each task. At any point during this wait, the user can hit escape to quit the program.

--- DesktopApp/TrackAndTrace/Program.cs
+++ DesktopApp/TrackAndTrace/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace TrackAndTrace
 {
@@ -10,13 +11,48 @@ namespace TrackAndTrace
             string outputFiles = @"C:\TrackAndTrace\OutputFiles";
             string viewingFiles = @"C:\TrackAndTrace\ViewingFiles";
 
-            Startup.PerformStartup(textMessagesLocalStorage, outputFiles, viewingFiles);
+            if (!Startup.PerformStartup(textMessagesLocalStorage, outputFiles, viewingFiles))
+            {
+                Console.WriteLine("Program was unable to create the necessary folders to start.");
+                Console.WriteLine("Please restart the program.");
+                Console.WriteLine("If this issue persists, please contact the developer.");
+                return;
+            }
+
+            bool programmingRunning = true;
+            Console.WriteLine("Please press the escape key if the program needs to be stopped.");
+            Console.WriteLine("This ensures that the program is not stopped during file transmission.");
+            Console.WriteLine("It could take up to 5 seconds for your key press to be registered, be patient.");
 
             FileRetriever fileRetriever = new FileRetriever(false, textMessagesLocalStorage);
-            fileRetriever.Run();
-            
             FileModifier fileModifier = new FileModifier(false, textMessagesLocalStorage, outputFiles);
-            fileModifier.Run();
+            FileComparer fileComparer = new FileComparer(false, outputFiles, viewingFiles);
+            Random random = new Random();
+            
+            while (programmingRunning)
+            {
+                fileRetriever.Run();    
+                fileModifier.Run();
+                fileComparer.Run();
+
+                //see if the user needs to stop the program while
+                //waiting a random time between 4 and 6 minutes to copy the files again.
+                int i = 0;
+                while (programmingRunning && i < 100)
+                {
+                    Thread.Sleep(random.Next(2800, 3200));
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKey key = Console.ReadKey().Key;
+                        programmingRunning = key != ConsoleKey.Escape;
+                    }
+
+                    i++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Program closing, press any key to continue.");
+            Console.ReadKey();
         }
     }
 }
